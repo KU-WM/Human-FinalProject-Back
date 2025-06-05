0644d78086c40f0eaef3439d2c78c2fb4c4a024a
KU-WM <ku990732@gmail.com>
2025-06-05 09:25:29 +0900

    edit cors option

--- src/main/java/com/backend/back/config/WebConfig.java
+++ src/main/java/com/backend/back/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://human-final-project-front.vercel.app/", "http://localhost:3000")
+                        .allowedOrigins("https://lnpra.com/", "http://localhost:3000")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
