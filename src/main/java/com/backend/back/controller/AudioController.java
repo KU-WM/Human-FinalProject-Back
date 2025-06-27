6eb36866cd0a7081efa63dbfa3c328cf2d7aa572
KU-WM <ku990732@gmail.com>
2025-06-27 12:13:19 +0900

    feat: create getAudios, getTempAudios, deleteAudio, deleteTempAudio
    create:
        getAudios : Get audios from image`s id
        getTempAudios : Get TempAudios from tempImage`s id
        deleteAudio : delete audio from audio`s id
        deleteTempAudio : get tempAudio from tempAudio`s id

--- src/main/java/com/backend/back/controller/AudioController.java
+++ src/main/java/com/backend/back/controller/AudioController.java
@@ -2,41 +2,95 @@ package com.backend.back.controller;
 
 import com.backend.back.dto.AudioDTO;
 import com.backend.back.mapper.AudioMapper;
-import com.backend.back.service.AudioService;
+import com.backend.back.service.DeleteFileService;
+import com.backend.back.service.GenerateAudioService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/audio")
 public class AudioController {
 
     @Autowired
-    private AudioService audioService;
+    private GenerateAudioService generateAudioService;
+
+    @Autowired
+    private DeleteFileService deleteFileService;
 
     @Autowired
     private AudioMapper audioMapper;
 
-    @ResponseBody
-    @GetMapping("/list")
+    @GetMapping("/list/{createFrom}")
+    public List<AudioDTO> getAudios(@PathVariable String createFrom) {
+        return audioMapper.getImageAudios(Integer.parseInt(createFrom));
+    }
+
+    @GetMapping("/templist/{createFrom}")
+    public List<AudioDTO> getTempAudios(@PathVariable String createFrom) {
+        return audioMapper.getTempImageAudios(Integer.parseInt(createFrom));
+    }
+
+    @GetMapping("/listall")
     public List<AudioDTO> getAllAudios() {
         return audioMapper.getAudios();
     }
 
-    @ResponseBody
+    @PostMapping("/delete")
+    public void deleteAudio(@RequestBody HashMap<String, String> message) {
+        int id = Integer.parseInt(message.get("id"));
+        AudioDTO audio = audioMapper.getAudio(id);
+
+        deleteFileService.deleteFile(audio.getSavePath());
+
+        audioMapper.deleteAudio(id);
+    }
+
+    @PostMapping("/tempdelete")
+    public void deleteTempAudio(@RequestBody HashMap<String, String> message) {
+        int id = Integer.parseInt(message.get("id"));
+        AudioDTO audio = audioMapper.getTempAudio(id);
+
+        deleteFileService.deleteFile(audio.getSavePath());
+
+        audioMapper.deleteTempAudioFromId(id);
+    }
+
+    // 입력: 유저 입력(message) + 해당 이미지 pk(imageId) / 출력: audioDTO
     @PostMapping("/generate")
-    public Object test(@RequestBody HashMap<String, Object> message) {
+    public Object test(@RequestBody HashMap<String, Object> message, HttpServletRequest request) {
         JSONParser parser = new JSONParser();
         AudioDTO audioDTO = new AudioDTO();
 
-        return audioService.generateAudioPrompt(message.get("message").toString())
-                .map(response -> {
+        return generateAudioService.generateAudioPrompt(message.get("message").toString())
+            .map(response -> {
+                JSONObject output = null;
+
+                try {
+                    output = (JSONObject) parser.parse(response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                JSONArray candidates = (JSONArray) output.get("candidates");
+                JSONObject candidate = (JSONObject) candidates.get(0);
+                JSONObject content = (JSONObject) candidate.get("content");
+                JSONArray parts = (JSONArray) content.get("parts");
+                JSONObject part = (JSONObject) parts.get(0);
+
+                return part.get("text").toString();
+            })
+            .flatMap(prompt -> {
+                return generateAudioService.generateAudio(prompt).map(response -> {
+                    audioDTO.setUserInput(message.get("message").toString());
+                    audioDTO.setPrompt(prompt);
+
                     JSONObject output = null;
 
                     try {
@@ -45,33 +99,23 @@ public class AudioController {
                         e.printStackTrace();
                     }
 
-                    JSONArray candidates = (JSONArray) output.get("candidates");
-                    JSONObject candidate = (JSONObject) candidates.get(0);
-                    JSONObject content = (JSONObject) candidate.get("content");
-                    JSONArray parts = (JSONArray) content.get("parts");
-                    JSONObject part = (JSONObject) parts.get(0);
+                    audioDTO.setSaveName(output.get("file_name").toString());
+                    audioDTO.setSavePath(output.get("save_path").toString());
 
-                    return part.get("text").toString();
-                })
-                .flatMap(prompt -> {
-                    return audioService.generateAudio(prompt).map(response -> {
-                        audioDTO.setUserInput(message.get("message").toString());
-                        audioDTO.setPrompt(prompt);
+                    int sourceImageId = Integer.parseInt(message.get("imageId").toString());
+                    audioDTO.setCreateFrom(sourceImageId);
 
-                        JSONObject output = null;
-
-                        try {
-                            output = (JSONObject) parser.parse(response);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                        audioDTO.setSaveName(output.get("file_name").toString());
-                        audioDTO.setSavePath(output.get("save_path").toString());
+                    if(request.getAttribute("isLogin") == "true" && message.getOrDefault("isOwner", "True").equals("True")) {
+                        audioDTO.setCreateBy(Integer.parseInt(request.getAttribute("id").toString()));
                         audioMapper.saveAudio(audioDTO);
+                    }
+                    else {
+                        audioDTO.setTempUserUUID(request.getAttribute("userId").toString());
+                        audioMapper.saveTempAudio(audioDTO);
+                    }
 
-                        return output.get("file_name").toString();
-                    });
+                    return audioDTO;
                 });
+            });
     }
 }
