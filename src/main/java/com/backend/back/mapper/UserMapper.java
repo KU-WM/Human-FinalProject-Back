8c31cb5ac31c9c7f0fb0e31b0f6a2d2966ccc856
KU-WM <ku990732@gmail.com>
2025-06-27 14:02:43 +0900

    feat: Add some function at UserMapper
    details:
    - findById : Get User data with user`s id
    - uuidSetting : Set UUID data in DB
    - uuidMatching : Matching TempUser`s UUID and User`s id
    - findUuid : Get User with it`s UUID
    - updateUserGrade : Update User`s grade with user`s id
    - deleteUser : Delete user with user`s id

--- src/main/java/com/backend/back/mapper/UserMapper.java
+++ src/main/java/com/backend/back/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.backend.back.mapper;
 
 import com.backend.back.dto.UserDTO;
+import com.backend.back.dto.UuidToUserDTO;
 import org.apache.ibatis.annotations.*;
 
 @Mapper
@@ -8,9 +9,27 @@ public interface UserMapper {
     @Select("select * from users where userId = #{userId}")
     UserDTO findByUserId(String userId);
 
+    @Select("select userId from users where id = #{id}")
+    String findById(int id);
+
     @Insert("Insert into users(userId, nickName, userPw) Values(#{userId}, #{nickName}, #{userPw})")
     void register(UserDTO user);
 
     @Update("Update users Set lastLogin = now() where userId = #{userId}")
     void lastLogin(String userId);
+
+    @Insert("Insert into uuidToUser(uuid) value(#{uuid})")
+    void uuidSetting(String uuid);
+
+    @Update("Update uuidToUser Set userId = #{id} where uuid = #{uuid}")
+    void uuidMatching(int id, String uuid);
+
+    @Select("Select * From uuidToUser where uuid = #{uuid}")
+    UuidToUserDTO findUuid(String uuid);
+
+    @Update("update users set userGrade = #{grade} where id = #{id}")
+    void updateUserGrade(int grade, int id);
+
+    @Delete("Delete FROM users where id = #{id}")
+    void deleteUser(int id);
 }
