9dc705209b1f6605348a354eeb782601422fc285
KU-WM <ku990732@gmail.com>
2025-06-27 12:23:46 +0900

    feat: create More AudioMapper`s function
    field:
        getAudio: Get audio from audio`s id
        deleteAudio : Delete audio from audio`s id
        getImageAudios : Get audios from base Image`s id
        getTempUserAudios : Get All TempAudios create from User`s UUID
        saveTempAudio : Save audio create by not logged in user
        deleteTempAudio : Delete tempAudio from user`s UUID
        deleteAllTempAudio : Delete All tempAudio
        getTempImageAudios : Get Audios from base tempImage`s id
        deleteTempAudioFromId : Delete tempAudio from it`s id
        getTempAudio : Get tempAudio from it`s id

--- src/main/java/com/backend/back/mapper/AudioMapper.java
+++ src/main/java/com/backend/back/mapper/AudioMapper.java
@@ -1,10 +1,7 @@
 package com.backend.back.mapper;
 
 import com.backend.back.dto.AudioDTO;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -13,7 +10,38 @@ public interface AudioMapper {
     @Select("Select * from audios Order by id DESC")
     List<AudioDTO> getAudios();
 
-    @Insert("INSERT INTO audios(userInput, prompt, savePath, saveName) VALUES (#{userInput}, #{prompt}, #{savePath}, #{saveName})")
+    @Insert("INSERT INTO audios(userInput, prompt, savePath, saveName, createBy, createFrom) VALUES (#{userInput}, #{prompt}, #{savePath}, #{saveName}, #{createBy}, #{createFrom})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void saveAudio(AudioDTO audioDTO);
+
+    @Select("Select * From audios Where id = #{id}")
+    AudioDTO getAudio(int id);
+
+    @Delete("Delete FROM audios where id = #{id}")
+    void deleteAudio(int id);
+
+    @Select("Select * from audios where createFrom = #{createFrom} Order by id DESC")
+    List<AudioDTO> getImageAudios(int createFrom);
+
+    @Select("SELECT * FROM tempAudios where tempUserUUID = #{userUUID} and createFrom = #{id} ORDER BY id DESC")
+    List<AudioDTO> getTempUserAudios(String userUUID, int id);
+
+    @Insert("INSERT INTO tempAudios(userInput, prompt, savePath, saveName, tempUserUUID, createFrom) VALUES (#{userInput}, #{prompt}, #{savePath}, #{saveName}, #{tempUserUUID}, #{createFrom})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void saveTempAudio(AudioDTO tempAudioDTO);
+
+    @Delete("Delete FROM tempAudios where tempUserUUID = #{tempUserUUID}")
+    void deleteTempAudio(String tempUserUUID);
+
+    @Delete("Delete FROM tempAudios where id > 0")
+    void deleteAllTempAudio();
+
+    @Select("Select * from tempAudios where createFrom = #{createFrom} Order by id DESC")
+    List<AudioDTO> getTempImageAudios(int createFrom);
+
+    @Delete("Delete FROM tempAudios where id = #{id}")
+    void deleteTempAudioFromId(int id);
+
+    @Select("Select * From tempAudios Where id = #{id}")
+    AudioDTO getTempAudio(int id);
 }
