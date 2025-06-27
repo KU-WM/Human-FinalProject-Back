efc0fc6343f098dba90a4b46e72483913c5b80b9
KU-WM <ku990732@gmail.com>
2025-06-27 12:49:40 +0900

    perf: delete unnecessary block

--- src/main/java/com/backend/back/controller/RefreshTokenController.java
+++ src/main/java/com/backend/back/controller/RefreshTokenController.java
@@ -2,31 +2,32 @@ package com.backend.back.controller;
 
 import com.backend.back.config.CustomUserDetailsService;
 import com.backend.back.config.JwtUtil;
+import com.backend.back.dto.UserDTO;
+import com.backend.back.mapper.UserMapper;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/token")
 public class RefreshTokenController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     private final JwtUtil jwtUtil;
-    private final CustomUserDetailsService customUserDetailsService;
 
     public RefreshTokenController(JwtUtil jwtUtil, CustomUserDetailsService customUserDetailsService) {
         this.jwtUtil = jwtUtil;
-        this.customUserDetailsService = customUserDetailsService;
     }
 
     @PostMapping("/refresh")
@@ -51,16 +52,14 @@ public class RefreshTokenController {
         System.out.println("3");
         // 2. refreshToken에서 username 추출
         Claims claim = jwtUtil.validateToken(refreshToken);
-        String username = claim.getSubject();
+        String id = claim.getSubject();
         System.out.println("4");
         // 3. 유저 정보 조회 및 권한 가져오기
-        UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
-        String roles = userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+        UserDTO user = userMapper.findByUserId(id);
+
         System.out.println("5");
         // 4. 새로운 Access Token 생성
-        String newAccessToken = jwtUtil.generateToken(username, roles);
+        String newAccessToken = jwtUtil.generateToken(user.getUserId(), Integer.toString(user.getUserGrade()), user.getId());
         System.out.println("6");
         // 5. 응답에 새 Access Token 전달
         return ResponseEntity.ok(Collections.singletonMap("accessToken", newAccessToken));
